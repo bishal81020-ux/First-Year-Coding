158ef5b271c480a58c44e3fcc99b9e7468498219
bishal81020-ux <bishal81020@gmail.com>
2026-04-18 04:51:56 +0530

    Create My_3rd_Program.c

---
+++ My_3rd_Program.c
@@ -0,0 +1,12 @@
+//This program is for multiplicitaion and subtraction
+#include<stdio.h>
+int main(){
+  float a, b ;
+  printf("Type your 1st no. for subtration\n" );
+  scanf("%f",&a);
+  printf("Type your 2nd no. for subtraction\n");
+  scanf("%f",&b);
+  printf("subtration of both no. is %f \n", a-b); 
+  printf("Multiplication of both no. is %f",a*b);
+  return 0;
+}}
