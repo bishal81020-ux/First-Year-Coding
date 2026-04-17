13dee62d45943752076bcb1f8e99d794b8f42806
bishal81020-ux <bishal81020@gmail.com>
2026-04-18 04:53:17 +0530

    Create My_5th_program.c

---
+++ My_5th_program.c
@@ -0,0 +1,10 @@
+//This program is to calculate the area of Circle
+#include<stdio.h>
+int main(){
+  float radius;
+  printf("Heyy you want to know the area of your balls. \n");
+  printf("Then write the radius of one ball \n"); 
+  scanf("%f",&radius);
+  printf("Hahahah you have small balls your ball is only %f", 3.14 * radius * radius);
+  return 0;
+}
