e2ffda0308d52410516305e9b807375619ec0530
bishal81020-ux <bishal81020@gmail.com>
2026-04-18 04:50:04 +0530

    Create My_2nd_program.c

---
+++ My_2nd_program.c
@@ -0,0 +1,12 @@
+//Adding numbers 
+#include<stdio.h>
+int main(){
+  int a, b;
+  printf("Heyy this is my 2nd program of adding numbers \n");
+  printf("Please type your 1st no.\n");
+  scanf("%d", &a);
+  printf("Please Type your 2nd no.\n");
+  scanf("%d", &b);
+  printf("Sum of the no. is = %d ",a+b);
+ return 0;
+}
