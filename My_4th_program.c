84cfc5e17ead75c62adb32a8e679ee1d4fd64918
bishal81020-ux <bishal81020@gmail.com>
2026-04-18 04:52:35 +0530

    Create My_4th_program.c

---
+++ My_4th_program.c
@@ -0,0 +1,10 @@
+//This program is to calculate the area of square
+#include<stdio.h>
+int main (){
+  int side;
+  
+  printf("Write the side of square\n");
+  scanf("%d",&side);
+  printf("The area of square = %d", side * side);
+  return 0;
+} 
