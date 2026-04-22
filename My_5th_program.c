dc457d5a121ec4a3d30c7f7d0fac220543936a40
bishal81020-ux <bishal81020@gmail.com>
2026-04-23 04:40:29 +0530

    My_5th_program.c

--- My_5th_program.c
+++ My_5th_program.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 int main(){
   float radius;
-  printf("Heyy you want to know the area of your balls. \n");
+  printf("Heyy you want to know the area of your eye balls. \n");
   printf("Then write the radius of one ball \n"); 
   scanf("%f",&radius);
-  printf("Hahahah you have small balls your ball is only %f", 3.14 * radius * radius);
+  printf("Hahahah you have small eye balls your ball is only %f", 3.14 * radius * radius);
   return 0;
 }
