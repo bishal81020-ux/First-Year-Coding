a5ed57bf0eff1109cf2c43e53673e1ec865cacce
bishal81020-ux <bishal81020@gmail.com>
2026-06-01 16:27:03 +0530

    My_17th_program.c

--- My_17th_program.c
+++ My_17th_program.c
@@ -1,1 +1,18 @@
-
+//This program is to check if the numbers entered by user is even or odd.
+#include<stdio.h>
+int main(){
+  printf("This is program is to check if the number you enter is even or odd.\n");
+  int n;
+  while(n%2==0){
+    printf("Enter your no:");
+    scanf("%d",&n);
+    if(n%2!=0){
+      printf("Acces Denied : Odd number detected!\n");
+      break;
+    }
+    else{
+      printf("Good job,\n");
+    }
+  }
+  return 0;
+}
